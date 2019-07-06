ee70e55953116ded12e5cd95e10eec601084d2f8
Harrison Hesslink <harrisonhesslink24@gmail.com>
2019-07-05 23:17:06 -0700

    Add Table and pricing

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+
+
+
+    $('#tradeTable').DataTable();
     $( "#buy" ).on( "click", function() {
         var seconds = Math.floor(new Date() / 1000);
         var data = {
@@ -25,4 +29,33 @@ $( document ).ready(function() {
         });
         
     })
+    setInterval(function(){
+        
+
+        $.get("/api/get_trades", function(result){
+        $.get("https://blockchain.info/ticker", function(result2){
+            var btcusd = 0;
+
+            btcusd = result2["USD"]["15m"];
+            var xtriusd = (btcusd * parseFloat(result[0]['Price'])).toFixed(4);
+            $("tbody").empty();
+            $("#sats").html("Satoshis: " + result[0]['Price'])
+            $("#usd").html("USD: " + xtriusd);
+            for(var i = 0; i < result.length;i++){
+                $("tbody").append(
+                    '<tr>' +
+                    '<td>' + result[i]["TimeStamp"] + '</td>' +
+                    '<td>' + result[i]["TradeType"] + '</td>' +
+                    '<td>' + result[i]["Amount"] + ' XTRI</td>' +
+                    '<td>' + result[i]["Price"] + ' BTC</td>' +
+                    '<td>' + (parseFloat(result[i]["Price"]) * parseFloat(result[i]["Amount"])).toString() + ' BTC </td>' +
+                    '</tr>'
+                )
+            }
+        })
+
+        });
+        $('#tradeTable').DataTable();
+    },15000)
+
 })
